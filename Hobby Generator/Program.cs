7c77a76699ae5cc19475b83b11d23b78869738c0
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-11-20 12:25:08 +0100

    first

--- Hobby Generator/Program.cs
+++ Hobby Generator/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Progam
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
 
+        int num = random.Next(1, 7);
+
+        if (num == 1)
+        {
+            Console.WriteLine("Ny hobby: Les en ny bok");
+        }
+        else if (num == 2)
+        {
+            Console.WriteLine("Ny hobby: Tren til ultramarathon");
+        }
+        else if (num == 3)
+        {
+            Console.WriteLine("Ny hobby: Progge nettsider");
+        }
+        else
+        {
+            Console.WriteLine("Ny hobby: Styrketrening");
+        }
+    }
+}
